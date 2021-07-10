37c9ceeaf5b6a1ffa802f8c362306ca38306a32f
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-07-10 02:08:50 +0000

    made basic api

--- api_app/views.py
+++ api_app/views.py
@@ -7,7 +7,15 @@ from django.http import JsonResponse
 
 
 
-def getListOfTasks(request):
-    
-    response = JsonResponse(json)
+def getListOfTasks(request): #only handeling GET requests 
+    json = {}# python
+    json['task'] = 'make api'# python
+    json['task2'] ='learn fetch ' # python
+    #2 ask db for specific stuff 
+    response = JsonResponse(json) # python to javascript because browser only speaks JS
     return response
+
+    #1 POST request
+# 1 seperate your backend and frontend - 
+# 2 if backend stops for a while - we stil have front end working 
+# 3 devide the backend  parts  - api 
